65178fc1d3ac4f1e604b78f4c7774f38da2bf55a
Ali Onur Uyar <aouyar@gmail.com>
2012-02-21 03:02:27 -0600

    Improve log format.

--- pybackup.py
+++ pybackup.py
@@ -25,16 +25,38 @@ __status__ = "Development"
 
 # Defaults
 defaultConfPaths = ['./pybackup.conf', '/etc/pybackup.conf']
-logLevel = logging.INFO
+defaultLogLevel = logging.INFO
+defaultLogFormat = '%(asctime)s:%(job)s:%(levelname)s %(message)s'
+defaultDateFormat = '%Y-%m-%d %H:%M:%S'
 
 # Global Default Settings
 defaultsGeneral = { 'cmd_gzip': 'gzip', 
                    }
 
 # Logging Configuration
-logging.basicConfig()
+logging.basicConfig(format=defaultLogFormat,
+                    datefmt=defaultDateFormat)
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(defaultLogLevel)
+
+
+class LogContext(logging.Filter):
+    
+    def __init__(self, job_name):
+        self._jobName = job_name
+        logging.Filter.__init__(self)
+    
+    def setJob(self, job_name):
+        self._jobName = job_name
+    
+    def filter(self, record):
+        record.job = self._jobName
+        return True
+        
+logContext = LogContext('INIT')
+logger.addFilter(logContext)        
+    
+
 
 class BackupError(Exception):
     pass
@@ -157,9 +179,6 @@ backupMethodRegistry = BackupMethodRegistry()
 
 
 
-
-
-
 defaultsPg = { 'job_name': 'PostgreSQL Backup',
                'cmd_pg_dump': 'pg_dump',
                'cmd_pg_dumpall': 'pg_dumpall',
@@ -188,7 +207,7 @@ class PgDumper():
                                  self._conf['filename_dump_globals'])
         args1 = [self._conf['cmd_pg_dumpall'], '-g']
         args2 = [self._conf['cmd_gzip'], '-c']
-        logger.info("PgDumper: Starting PostgreSQL Global Objects dump."
+        logger.info("Starting PostgreSQL Global Objects dump."
                     "  Backup: %s", dump_path)
         try:
             cmd1 = subprocess.Popen(args1, 
@@ -196,13 +215,13 @@ class PgDumper():
                                     stderr=subprocess.PIPE, 
                                     bufsize=defaultBufferSize)
         except Exception, e:
-            raise BackupError("PgDumper: Database dump command failed.",
+            raise BackupError("Database dump command failed.",
                               "Command: %s" % ' '.join(args1),
                               "Error Message: %s" % str(e))
         try:
             outfile = os.open(dump_path, os.O_WRONLY | os.O_CREAT)
         except Exception, e:
-            raise BackupError("PgDumper: Failed creation of backup file.",
+            raise BackupError("Failed creation of backup file.",
                              "Error Message: %s" % str(e))
         try:
             cmd2 = subprocess.Popen(args2,
@@ -212,21 +231,21 @@ class PgDumper():
                                     bufsize=defaultBufferSize)
             cmd1.stdout.close()
         except Exception, e:
-            raise BackupError("PgDumper: Backup compression command failed.",
+            raise BackupError("Backup compression command failed.",
                               "Command: %s" % ' '.join(args2),
                               "Error Message: %s" % str(e))
         errdata2 = cmd2.communicate(None)[1]
         errdata1 = cmd1.stderr.read()
         cmd1.wait()
         if cmd1.returncode == 0 and cmd2.returncode == 0:
-            logger.info("PgDumper: Finished PostgreSQL Global Objects dump. "
-                        "Backup: %s", dump_path)
+            logger.info("Finished PostgreSQL Global Objects dump."
+                        "  Backup: %s", dump_path)
         elif cmd1.returncode != 0:
-            raise BackupError("PgDumper: Dump failed with error code %s." 
+            raise BackupError("Dump failed with error code %s." 
                               % cmd1.returncode,
                               *errdata1.splitlines())
         elif cmd2.returncode != 0:
-            raise BackupError("PgDumper: Compression of dump failed "
+            raise BackupError("Compression of dump failed "
                               "with error code %s." % cmd2.returncode,
                               *errdata2.splitlines())
 
@@ -235,22 +254,22 @@ class PgDumper():
                                         db)
         dump_path = os.path.join(self._conf['backup_path'], dump_filename)
         args = [self._conf['cmd_pg_dump'], '-Fc', '-f', dump_path, db]
-        logger.info("PgDumper: Starting dump of PostgreSQL Database: %s"
+        logger.info("Starting dump of PostgreSQL Database: %s"
                     "  Backup: %s", db, dump_path)
         try:
             cmd = subprocess.Popen(args, 
                                    stderr=subprocess.PIPE, 
                                    bufsize=defaultBufferSize)
         except Exception, e:
-            raise BackupError("PgDumper: Database dump command failed.",
+            raise BackupError("Database dump command failed.",
                               "Command: %s" % ' '.join(args),
                               "Error Message: %s" % str(e))
         errdata = cmd.communicate(None)[1]
         if cmd.returncode == 0:
-            logger.info("PgDumper: Finished dump of PostgreSQL Database: %s"
+            logger.info("Finished dump of PostgreSQL Database: %s"
                         "  Backup: %s", db, dump_path)
         else:
-            raise BackupError("PgDumper: Dump of database %s failed "
+            raise BackupError("Dump of database %s failed "
                               "with error code %s." % (db, cmd.returncode),
                               *errdata.splitlines())
     
@@ -261,18 +280,18 @@ class PgDumper():
                 self._conf['db_list'] = pg.getDatabases()
                 del pg
             except Exception, e:
-                raise BackupError("PgDumper: Connection to PostgreSQL Server "
+                raise BackupError("Connection to PostgreSQL Server "
                                   "for querying database list failed.",
                                   "Error Message: %s" % str(e))
         try:
             self._conf['db_list'].remove('template0')
         except ValueError:
             pass
-        logger.info("PgDumper: Starting dump of %d PostgreSQL Databases.",
+        logger.info("Starting dump of %d PostgreSQL Databases.",
                     len(self._conf['db_list']))
         for db in self._conf['db_list']:
             self.pgDumpDatabase(db)
-        logger.info("PgDumper: Finished dump of PostgreSQL Databases.")
+        logger.info("Finished dump of PostgreSQL Databases.")
 
     def pgDumpFull(self):
         self.pgDumpGlobals()
@@ -308,30 +327,29 @@ def main(argv=None):
         logger.warning("No backup job selected for execution.")
         return 1
     for job_name in args:
+        logContext.setJob(job_name)
         try:
             job_conf = jobs_conf.get(job_name)
             if job_conf:
-                logger.info("Starting executing backup job: %s", job_name)
+                logger.info("Starting execution of backup job")
                 job = BackupJob(job_name, global_conf, job_conf)
                 job.run()
-                logger.info("Finished executing backup job: %s", job_name)
+                logger.info("Finished execution of backup job")
             else:
-                logger.error("No configuration found for backup job %s." % 
-                             job_name)
+                logger.error("No configuration found for backup job.")
         except BackupError, e:
             fatal = False
             if isinstance(e, BackupFatalConfigError):
-                logger.critical("Fatal configuration error in job: %s", job_name)
+                logger.critical("Fatal configuration error in job.")
                 fatal = True
             elif isinstance(e, BackupConfigError):
-                logger.error("Configuration error in backup job %s. %s", 
-                             job_name)
+                logger.error("Configuration error in backup job")
                 fatal = False
             elif isinstance(e, BackupEnvironmentError):
-                logger.error("Error in backup job %s environment.", job_name)
+                logger.error("Error in backup job environment.")
                 fatal = False
             else:     
-                logger.error("Error in execution of backup job %s.", job_name)
+                logger.error("Error in execution of backup job.")
                 fatal = False
             for line in e:
                 logger.error("  %s" , line)
